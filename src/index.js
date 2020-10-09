24121b6301f4268c62cd23eddd14d209a2078282
areebvohra <areebvohra@gmail.com>
2020-10-10 03:27:09 +0500

    google analytics added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-180251929-1'); // add your tracking id here.
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
