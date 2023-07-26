d053a0270536666ba95912f0a3ea323f162d9f35
Muhammad Areeb Vohra <areebvohra@gmail.com>
2023-07-26 18:22:00 +0400

    new changes made

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 
+const months = ["#fc4160", "#8c41fc", "#41d3fc", "#418ffc"];
 class Resume extends Component {
   render() {
 
     if (this.props.data) {
       var skillmessage = this.props.data.skillmessage;
-      var education = this.props.data.education.map(function (education) {
-        return <div key={education.school}><h3>{education.school}</h3>
+      var education = this.props.data.education.map((education) => (
+        <div key={education.school}>
+          <h3>{education.school}</h3>
           <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-          <p>{education.description}</p></div>
-      })
-      var work = this.props.data.work.map(function (work) {
-        return <div key={work.company}><h3>{work.company}</h3>
+          <p>{education.description}</p>
+        </div>
+      ))
+      var work = this.props.data.work.map((work) => (
+        <div key={work.company}>
+          <h3>{work.company}</h3>
           <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
           <p>{work.description}</p>
         </div>
-      })
-      var skills = this.props.data.skills.map(function (skills) {
-        var className = 'bar-expand ' + skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{ width: skills.level, textAlign: 'end', color: '#fff', background: skills.color, fontSize: '13px' }} className={className}>{skills.level}</span><em>{skills.name}</em></li>
-      })
+      ))
+      var skills = this.props.data.skills
     }
 
     return (
@@ -51,20 +52,20 @@ class Resume extends Component {
 
 
         <div className="row skill">
-
           <div className="three columns header-col">
             <h1><span>Skills</span></h1>
           </div>
 
           <div className="nine columns main-col">
-
             <p style={{ fontWeight: 'bold', color: '#313131', fontSize: 18 }}>{skillmessage}</p>
-
-            <div className="bars">
-              <ul className="skills">
-                {skills}
-              </ul>
-            </div>
+            {
+              skills && skills.map((item, index) => {
+                var randomNumber = Math.floor(Math.random() * months.length);
+                return (
+                  <div key={index} style={{ background: months[randomNumber] }} className="tag">{item}</div>
+                )
+              })
+            }
           </div>
         </div>
       </section>
